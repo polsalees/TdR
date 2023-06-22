5302c6238c73f9d47ef76249796bfc3952e17302
polsalees <itssupercled@gmail.com>
2023-06-22 16:05:24 +0200

    Li he canviat el nom al fitxer perque apareixin colors OHH YEAHHH

--- Videojoc a sublime.py
+++ Videojoc a sublime.py
@@ -13,6 +13,8 @@ blau = (0, 0, 255)
 cian = (0, 255, 255)
 rosa = (255, 0, 255)
 groc = (255, 255, 0)
+taronja = (255, 165, 0)
+fons = (80, 80, 255)
 
 # Preparar la pantalla
 pantalla_amplada, pantalla_alçada = 1920, 1038
@@ -64,34 +66,64 @@ def porcs():
     pygame.draw.circle(pantalla, verd, porc_posició, porc_radi)
 
 # Selecció de nivell
-def selecció_dificultat():
-    dificultat_seleccionada = None
-    while not dificultat_seleccionada:
+def selecció_nivell():
+    nivell_seleccionat = None
+    while not nivell_seleccionat:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_1:
-                    dificultat_seleccionada = "Fàcil"
+                    nivell_seleccionat = 1
                 elif event.key == pygame.K_2:
-                    dificultat_seleccionada = "Intermig"
+                    nivell_seleccionat = 2
                 elif event.key == pygame.K_3:
-                    dificultat_seleccionada = "Difícil"
+                    nivell_seleccionat = 3
+                elif event.key == pygame.K_4:
+                    nivell_seleccionat = 4
+                elif event.key == pygame.K_5:
+                    nivell_seleccionat = 5
+                elif event.key == pygame.K_6:
+                    nivell_seleccionat = 6
+                elif event.key == pygame.K_7:
+                    nivell_seleccionat = 7
+                elif event.key == pygame.K_8:
+                    nivell_seleccionat = 8
+                elif event.key == pygame.K_9:
+                    nivell_seleccionat = 9
+          
+        pantalla.fill(fons)
         
-        pantalla.fill(blanc)
-        
-        font = pygame.font.Font(None, 50)
-        text1 = font.render("Seleccioinar dificultat:", True, negre)
-        text2 = font.render("1 - Fàcil", True, negre)
-        text3 = font.render("2 - Intermig", True, negre)
-        text4 = font.render("3 - Difícil", True, negre)
+        font = pygame.font.Font(None, 100)
+        text1 = font.render("Seleccionar nivell:", True, taronja)
+        text2 = font.render("1", True, taronja)
+        text3 = font.render("2", True, taronja)
+        text4 = font.render("3", True, taronja)
+        text5 = font.render("4", True, taronja)
+        text6 = font.render("5", True, taronja)
+        text7 = font.render("6", True, taronja)
+        text8 = font.render("7", True, taronja)
+        text9 = font.render("8", True, taronja)
+        text10 = font.render("9", True, taronja)
+        text11= font.render("10", True, taronja)
+        text12 = font.render("11", True, taronja)
+        text13 = font.render("12", True, taronja)
 
-        pantalla.blit(text1, (pantalla_amplada // 2 - text1.get_width() // 2, pantalla_alçada // 2 - text1.get_height() // 2 - 75))
-        pantalla.blit(text2, (pantalla_amplada // 2 - text2.get_width() // 2, pantalla_alçada // 2 - text2.get_height() // 2 - 25))
-        pantalla.blit(text3, (pantalla_amplada // 2 - text3.get_width() // 2, pantalla_alçada // 2 - text3.get_height() // 2 + 25))
-        pantalla.blit(text4, (pantalla_amplada // 2 - text4.get_width() // 2, pantalla_alçada // 2 - text4.get_height() // 2 + 75))
+        pantalla.blit(text1, (pantalla_amplada // 2 - text1.get_width() // 2, pantalla_alçada // 5 - text1.get_height() // 2 ))
+        pantalla.blit(text2, (pantalla_amplada // 5 - text2.get_width() // 2, pantalla_alçada * 2 // 5 - text2.get_height() // 2 ))
+        pantalla.blit(text3, (pantalla_amplada * 2 // 5 - text3.get_width() // 2, pantalla_alçada * 2 // 5 - text3.get_height() // 2 ))
+        pantalla.blit(text4, (pantalla_amplada * 3 // 5 - text4.get_width() // 2, pantalla_alçada * 2 // 5 - text4.get_height() // 2 ))
+        pantalla.blit(text5, (pantalla_amplada * 4 // 5 - text5.get_width() // 2, pantalla_alçada * 2 // 5 - text5.get_height() // 2 ))
+        pantalla.blit(text6, (pantalla_amplada // 5 - text6.get_width() // 2, pantalla_alçada * 3 // 5 - text6.get_height() // 2 ))
+        pantalla.blit(text7, (pantalla_amplada * 2 // 5 - text7.get_width() // 2, pantalla_alçada * 3 // 5 - text7.get_height() // 2 ))
+        pantalla.blit(text8, (pantalla_amplada * 3 // 5 - text8.get_width() // 2, pantalla_alçada * 3 // 5 - text8.get_height() // 2 ))
+        pantalla.blit(text9, (pantalla_amplada * 4 // 5 - text9.get_width() // 2, pantalla_alçada * 3 // 5 - text9.get_height() // 2 ))
+        pantalla.blit(text10, (pantalla_amplada // 5 - text10.get_width() // 2, pantalla_alçada * 4 // 5 - text10.get_height() // 2 ))
+        pantalla.blit(text11, (pantalla_amplada * 2 // 5 - text11.get_width() // 2, pantalla_alçada * 4 // 5 - text11.get_height() // 2 ))
+        pantalla.blit(text12, (pantalla_amplada * 3 // 5 - text12.get_width() // 2, pantalla_alçada * 4 // 5 - text12.get_height() // 2 ))
+        pantalla.blit(text13, (pantalla_amplada * 4 // 5 - text13.get_width() // 2, pantalla_alçada * 4 // 5 - text13.get_height() // 2 ))
 
         pygame.display.flip()
 
-    return dificultat_seleccionada
+    return nivell_seleccionat
 
 # Menú principal
 def menú():
@@ -101,14 +133,14 @@ def menú():
                 if event.key == pygame.K_ESCAPE:
                     return False
                 elif event.key == pygame.K_SPACE:
-                    dificultat = selecció_dificultat()
+                    dificultat = selecció_nivell()
                     if dificultat:
                         return True
 
-        pantalla.fill(blanc)
+        pantalla.fill(fons)
 
-        font = pygame.font.Font(None, 50)
-        text = font.render("Angry Birds", True, negre)
+        font = pygame.font.Font(None, 300)
+        text = font.render("Angry Birds", True, taronja)
         pantalla.blit(text, (pantalla_amplada // 2 - text.get_width() // 2, pantalla_alçada // 2 - text.get_height() // 2))
 
         pygame.display.flip()
@@ -142,7 +174,7 @@ def GameLoop():
                 mantenint_ocell = True
         
             # Netejar la pantalla
-            pantalla.fill(blanc)
+            pantalla.fill(fons)
         
             # Aparèixer porcs, ocells i linea
             if mantenint_ocell:
